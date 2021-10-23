d840ca7a1a1940e339065b2c899dfded2447ce44
ANJALI KUMARI <91587176+A1ANJALIKUMARI@users.noreply.github.com>
2021-10-24 01:51:40 +0530

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,7 @@
+#importing calendar library
+
+import calendar
+year=int(input("enter a year:"))
+def Printcalendar(year):
+  Print(calendar.calendar(year))  #print calendar
+Printcalendar(year)                #driver program to test above function
